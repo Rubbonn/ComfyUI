f0369372fbe240bb3f408d8b477938b294a51aad
Ruben Giuriato <37040971+Rubbonn@users.noreply.github.com>
2025-08-02 15:09:47 +0200

    - Modificato entrypoint per installare anche i nodi custom

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,10 +9,19 @@ if [ -f '.venv/bin/activate' ]; then
 	. .venv/bin/activate
 else
 	python3 -m venv .venv \
-	&& . .venv/bin/activate \
-	&& pip install --upgrade pip \
+	&& . .venv/bin/activate
+fi
+
+# Install system dependencies
+pip install --upgrade pip \
 	&& pip install torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/cu128 \
 	&& pip install -r requirements.txt
-fi
+
+# Installa requirements.txt per ogni sottocartella di custom_nodes
+for d in custom_nodes/*/ ; do
+    if [ -f "$d/requirements.txt" ]; then
+        pip install -r "$d/requirements.txt"
+    fi
+done
 
 exec python3 main.py "$@"
