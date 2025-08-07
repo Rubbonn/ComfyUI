fbca84b8ac7f9304d6fbb4fc5e990092dfce122d
Ruben Giuriato <37040971+Rubbonn@users.noreply.github.com>
2025-08-08 01:57:37 +0200

    - Aggiunto Sageattention e Comfyui manager - Aggiunte due cartelle per la cache e triton

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,13 @@
 FROM debian:latest
 
-RUN apt-get update && apt-get install -y python3 python3-pip python3-venv wget curl git && apt-get clean
+RUN apt-get update && apt-get install -y python3 python3-pip python3-venv wget curl git
+RUN wget https://developer.download.nvidia.com/compute/cuda/repos/debian12/x86_64/cuda-keyring_1.1-1_all.deb \
+    && apt-get install -y ./cuda-keyring_1.1-1_all.deb \
+    && rm cuda-keyring_1.1-1_all.deb \
+    && apt-get update \
+    && apt-get install -y cuda-nvcc-12-9 libcusparse-dev-12-9 libcublas-dev-12-9 libcusolver-dev-12-9 \
+    && apt-get clean
+RUN mkdir -m 777 /.triton /.cache
 
 WORKDIR /app
 COPY . /app
--- entrypoint.sh
+++ entrypoint.sh
@@ -12,15 +12,35 @@ else
 	&& . .venv/bin/activate
 fi
 
-# Install system dependencies
-pip install --upgrade pip \
-	&& pip install torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/cu128 \
-	&& pip install -r requirements.txt
+# Installa dipendenze di sistema
+pip install --upgrade pip setuptools wheel \
+	&& pip install --upgrade torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/cu129 \
+	&& pip install -r requirements.txt || exit 1
+
+# Installa Sageattention
+if  ! pip show sageattention; then
+	git clone https://github.com/thu-ml/SageAttention.git /tmp/SageAttention \
+		&& pip install --no-build-isolation /tmp/SageAttention
+fi
+
+# Controlla se i ComfyUI Manager è già presente e aggiorna o clona se necessario
+if [ -d 'custom_nodes/ComfyUI-Manager' ]; then
+	(cd custom_nodes/ComfyUI-Manager && git pull)
+else
+	(cd custom_nodes && git clone https://github.com/Comfy-Org/ComfyUI-Manager.git)
+fi
+
+# Controlla se i wrapper Kijai sono già presenti e aggiorna o clona se necessario
+if [ -d 'custom_nodes/ComfyUI-WanVideoWrapper' ]; then
+	(cd custom_nodes/ComfyUI-WanVideoWrapper && git pull)
+else
+	(cd custom_nodes && git clone https://github.com/kijai/ComfyUI-WanVideoWrapper.git)
+fi
 
 # Installa requirements.txt per ogni sottocartella di custom_nodes
 for d in custom_nodes/*/ ; do
     if [ -f "$d/requirements.txt" ]; then
-        pip install -r "$d/requirements.txt"
+        pip install -r "$d/requirements.txt" || exit 1
     fi
 done
 
